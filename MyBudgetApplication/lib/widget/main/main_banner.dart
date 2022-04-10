aef7a29e79d5527c5a5e884f059bd5781d5dbede
Nikola Stanojkovski <65570806+nikolaStanojkovski@users.noreply.github.com>
2022-04-10 21:21:04 +0200

    Main files documentation.

--- MyBudgetApplication/lib/widget/main/main_banner.dart
+++ MyBudgetApplication/lib/widget/main/main_banner.dart
@@ -5,9 +5,12 @@ import 'package:my_budget_application/widget/main/main_spent_banner.dart';
 import '../../util/constants.dart';
 import '../image_banner.dart';
 
+/// Defines the header banner widget for the expense list.
 class MainBanner extends StatelessWidget {
+  /// The list of [Expense] objects to show the banner for.
   final List<Expense> _expenses;
 
+  /// Creates an instance of [MainBanner] with [_expenses].
   const MainBanner(this._expenses, {Key? key}) : super(key: key);
 
   @override
